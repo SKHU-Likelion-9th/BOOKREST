2c5fc8477951770bc6b1ad62ec5ce1c3b3376665
hanseulhee <3021062@gmail.com>
2021-08-01 19:27:53 +0900

    윈도우 가상환경 winvenv 생성, signup issue 해결

--- bookrest/join/views.py
+++ bookrest/join/views.py
@@ -1,34 +1,35 @@
 from django.shortcuts import redirect, render
 from django.contrib import auth
-from django.contrib.auth.forms import AuthenticationForm # 로그인, 회원가입 폼 
+from django.contrib.auth.forms import AuthenticationForm  # 로그인, 회원가입 폼
 from .forms import UserCreationForm
 # Create your views here.
-def mypage(request): #마이페이지
+
+
+def mypage(request):  # 마이페이지
     return render(request, 'mypage.html')
 
 
-
-
-def sign_up(request): #회원가입
+def sign_up(request):  # 회원가입
     context = {}
-    if request.method == 'POST': #Post 방식 
+    if request.method == 'POST':  # Post 방식
         form = UserCreationForm(request.POST)
-        if form.is_valid(): #유효성 검사 
+        if form.is_valid():  # 유효성 검사
+            form.save()
             email = form.cleaned_data.get('email')
             raw_password = form.cleaned_data.get('password1')
             user = auth.authenticate(email=email, password=raw_password)
             auth.login(request, user)
-            form.save()
             return redirect('main')
         else:
             context['signup_form'] = form
             # return render(request, 'sign_up.html', {'form':form})
-    else: #get 방식 
+    else:  # get 방식
         form = UserCreationForm()
         context['signup_form'] = form
-    return render(request, 'sign_up.html', context)
+    return render(request, 'sign_up.html', {"form": form})
 
-def sign_in(request): #로그인
+
+def sign_in(request):  # 로그인
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
@@ -41,6 +42,7 @@ def sign_in(request): #로그인
         form = AuthenticationForm()
         return render(request, 'sign_in.html', {'form': form})
 
-def sign_out(request): #로그아웃
+
+def sign_out(request):  # 로그아웃
     auth.logout(request)
     return redirect('main')
