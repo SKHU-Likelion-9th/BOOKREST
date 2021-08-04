bde21111443fd1c919d6ff8d4dbcf38ef781159f
hanseulhee <3021062@gmail.com>
2021-08-04 17:16:25 +0900

    main scroll, 문의하기 페이지 구성

--- bookrest/main/static/js/notice.js
+++ bookrest/main/static/js/notice.js
@@ -17,37 +17,6 @@ let currentIndex = 0;
 let beforeIndex = 0;
 const elementLinkList = [introduceA, ruleA, bookrestMapA, faqA];
 
-// const noticeAnimateIntroduce = () => {
-//   introduce.classList.add(NOTICESHOWING_CN);
-//   rule.classList.remove(NOTICESHOWING_CN);
-//   bookrestMap.classList.remove(NOTICESHOWING_CN);
-//   inquiry.classList.remove(NOTICESHOWING_CN);
-// };
-
-// const noticeAnimateRule = () => {
-//   rule.classList.add(NOTICESHOWING_CN);
-//   introduce.classList.remove(NOTICESHOWING_CN);
-//   // introduce.classList.remove(INTRODUCESHOW);
-//   bookrestMap.classList.remove(NOTICESHOWING_CN);
-//   inquiry.classList.remove(NOTICESHOWING_CN);
-// };
-
-// const noticeAnimateBookrestMap = () => {
-//   bookrestMap.classList.add(NOTICESHOWING_CN);
-//   introduce.classList.remove(NOTICESHOWING_CN);
-//   // introduce.classList.remove(INTRODUCESHOW);
-//   rule.classList.remove(NOTICESHOWING_CN);
-//   inquiry.classList.remove(NOTICESHOWING_CN);
-// };
-
-// const noticeAnimateInquiry = () => {
-//   inquiry.classList.add(NOTICESHOWING_CN);
-//   introduce.classList.remove(NOTICESHOWING_CN);
-//   // introduce.classList.remove(INTRODUCESHOW);
-//   bookrestMap.classList.remove(NOTICESHOWING_CN);
-//   rule.classList.remove(NOTICESHOWING_CN);
-// };
-
 const addClassNameToCurrentElement = (index) => {
   elementList[beforeIndex].classList.remove(NOTICESHOWING_CN);
   elementList[index].classList.add(NOTICESHOWING_CN);
@@ -60,10 +29,6 @@ const noticeInit = () => {
       addClassNameToCurrentElement(index);
     });
   });
-  // introduceA.addEventListener("click", noticeAnimateIntroduce);
-  // ruleA.addEventListener("click", noticeAnimateRule);
-  // bookrestMapA.addEventListener("click", noticeAnimateBookrestMap);
-  // inquiryA.addEventListener("click", noticeAnimateInquiry);
 };
 
 noticeInit();
