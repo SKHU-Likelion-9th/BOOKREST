372e0c92cb464d19e822a233fea1669322aacb42
hasungYu <dbgktjd@naver.com>
2021-08-05 18:01:52 +0900

    category 작업중

--- bookrest/category/static/js/category.js
+++ bookrest/category/static/js/category.js
@@ -5,17 +5,35 @@ $('#get_all').click(function() {
   $("#society").hide();
   $("#media_content").hide();
   $("#it").hide();
+
+  //카테고리 색상
+  $("#get_all").css("background-color","#2DBE61");
+  $("#get_humanities").css("background-color","#808080");
+  $("#get_society").css("background-color","#808080");
+  $("#get_media_content").css("background-color","#808080");  
+  $("#get_it").css("background-color","#808080");
+
 })
 
 
 //인문 navbar
 $('#get_humanities').click(function() {
-    $("#all").hide();
-    $("#humanities").show();
-    $("#society").hide();
-    $("#media_content").hide();
-    $("#it").hide();
+  $("#all").hide();
+  $("#humanities").show();
+  $("#society").hide();
+  $("#media_content").hide();
+  $("#it").hide();
+
+  //카테고리 색상
+  $("#get_humanities").css("background-color","#2DBE61");
+  $("#get_all").css("background-color","#808080");
+  $("#get_society").css("background-color","#808080");
+  $("#get_media_content").css("background-color","#808080");  
+  $("#get_it").css("background-color","#808080");
+  
 })
+
+
 
 //사회 navbar
 $('#get_society').click(function() {
@@ -24,6 +42,15 @@ $('#get_society').click(function() {
   $("#society").show();
   $("#media_content").hide();
   $("#it").hide();
+
+  //카테고리 색상
+  
+  $("#get_society").css("background-color","#2DBE61");
+  $("#get_all").css("background-color","#808080");
+  $("#get_humanities").css("background-color","#808080");
+  $("#get_media_content").css("background-color","#808080");  
+  $("#get_it").css("background-color","#808080");
+  
 })
 
 //미컨 navbar
@@ -33,6 +60,14 @@ $('#get_media_content').click(function() {
   $("#society").hide();
   $("#media_content").show();
   $("#it").hide();
+
+  //카테고리 색상
+  $("#get_media_content").css("background-color","#2DBE61");
+  $("#get_all").css("background-color","#808080");
+  $("#get_humanities").css("background-color","#808080");
+  $("#get_society").css("background-color","#808080");
+  $("#get_it").css("background-color","#808080");
+
 })
 
 //it navbar
@@ -42,4 +77,12 @@ $('#get_it').click(function() {
   $("#society").hide();
   $("#media_content").hide();
   $("#it").show();
+
+  //카테고리 색상
+  
+  $("#get_it").css("background-color","#2DBE61");
+  $("#get_all").css("background-color","#808080");
+  $("#get_humanities").css("background-color","#808080");
+  $("#get_society").css("background-color","#808080");
+  $("#get_media_content").css("background-color","#808080");  
 })
