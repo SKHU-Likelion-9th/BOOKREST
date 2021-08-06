2fee3d87f50f5c4d0d79a136de3db1f7ca2f40ba
hasungYu <dbgktjd@naver.com>
2021-08-06 17:48:23 +0900

    category 데이터 받아오는 작업중

--- bookrest/category/views.py
+++ bookrest/category/views.py
@@ -3,5 +3,5 @@ from .models import BookClassInfo
 
 # Create your views here.
 def category(request):
-    categorys = BookClassInfo.objects
+    categorys = BookClassInfo.objects.all()
     return render(request, 'category.html', {'categorys':categorys})
