231e78ecef0e988858ecebcf248e7d796240b3af
hasungYu <dbgktjd@naver.com>
2021-08-06 21:44:57 +0900

    category 작업중ㅇ

--- bookrest/category/views.py
+++ bookrest/category/views.py
@@ -1,7 +1,20 @@
+from django.core import paginator
 from django.shortcuts import render
 from .models import BookClassInfo
+from django.core.paginator import Paginator
 
 # Create your views here.
+# 검색과 페이징
 def category(request):
-    categorys = BookClassInfo.objects.all()
-    return render(request, 'category.html', {'categorys':categorys})
+    
+    books = BookClassInfo.objects.all()
+    search = request.GET.get('search', '')
+
+    if search:
+        books = books.filter(title__icontains=search)
+
+    paginator = Paginator(books, 9)
+    page = request.GET.get('page', '') #몇번째 페이지인지 받아옴
+    book_list = paginator.get_page(page) #posts
+
+    return render(request, 'category.html', {'book_list':book_list, 'search':search })
