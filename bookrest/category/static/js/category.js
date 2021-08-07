8b7bdfaf6cdb335d72f8017914bc18a37f851e3c
hasungYu <dbgktjd@naver.com>
2021-08-07 13:55:46 +0900

    category cs거의완료

--- bookrest/category/static/js/category.js
+++ bookrest/category/static/js/category.js
@@ -1,10 +1,5 @@
 //all navbar
 $('#get_all').click(function() {
-  $("#all").show();
-  $("#humanities").hide();
-  $("#society").hide();
-  $("#media_content").hide();
-  $("#it").hide();
 
   //카테고리 색상
   $("#get_all").css("background-color","#2DBE61");
@@ -18,13 +13,7 @@ $('#get_all').click(function() {
 
 //인문 navbar
 $('#get_humanities').click(function() {
-  $("#all").hide();
-  $("#humanities").show();
-  $("#society").hide();
-  $("#media_content").hide();
-  $("#it").hide();
   
-
   //카테고리 색상
   $("#get_humanities").css("background-color","#2DBE61");
   $("#get_all").css("background-color","#808080");
@@ -32,25 +21,14 @@ $('#get_humanities').click(function() {
   $("#get_media_content").css("background-color","#808080");  
   $("#get_it").css("background-color","#808080");
   
-  
-
- 
-  
-
 })
 
 
 
 //사회 navbar
 $('#get_society').click(function() {
-  $("#all").hide();
-  $("#humanities").hide();
-  $("#society").show();
-  $("#media_content").hide();
-  $("#it").hide();
 
   //카테고리 색상
-  
   $("#get_society").css("background-color","#2DBE61");
   $("#get_all").css("background-color","#808080");
   $("#get_humanities").css("background-color","#808080");
@@ -61,11 +39,6 @@ $('#get_society').click(function() {
 
 //미컨 navbar
 $('#get_media_content').click(function() {
-  $("#all").hide();
-  $("#humanities").hide();
-  $("#society").hide();
-  $("#media_content").show();
-  $("#it").hide();
 
   //카테고리 색상
   $("#get_media_content").css("background-color","#2DBE61");
@@ -78,12 +51,6 @@ $('#get_media_content').click(function() {
 
 //it navbar
 $('#get_it').click(function() {
-  $("#all").hide();
-  $("#humanities").hide();
-  $("#society").hide();
-  $("#media_content").hide();
-  $("#it").show();
-
   //카테고리 색상
   
   $("#get_it").css("background-color","#2DBE61");
@@ -91,4 +58,5 @@ $('#get_it').click(function() {
   $("#get_humanities").css("background-color","#808080");
   $("#get_society").css("background-color","#808080");
   $("#get_media_content").css("background-color","#808080");  
+  
 })
