2e38f57d031eecf8db2c323f3347aa39392c282c
hasungYu <dbgktjd@naver.com>
2021-08-07 19:01:39 +0900

    넘어갑니다

--- bookrest/vote/static/js/vote.js
+++ bookrest/vote/static/js/vote.js
@@ -1,17 +1,89 @@
-$(function(){
-    $("#confirm").click(function(){
-        modalClose(); //모달 닫기 함수 호출
-        
-        //컨펌 이벤트 처리
-    });
-    $("#modal-open").click(function(){        
-        $("#popup").css('display','flex').hide().fadeIn();
-        //팝업을 flex속성으로 바꿔준 후 hide()로 숨기고 다시 fadeIn()으로 효과
-    });
-    $("#close").click(function(){
-        modalClose(); //모달 닫기 함수 호출
-    });
-    function modalClose(){
-        $("#popup").fadeOut(); //페이드아웃 효과
+$("#modal-open").click(function(){
+    if(confirm("투표 하시겠습니까?") == true){
+        alert("투표가 완료되었습니다");
+        document.getElementById("img").src = "/static/img/onclick.png";
     }
-  });
+    else{
+        return ;
+    }
+});
+
+$("#modal-open1").click(function(){
+    if(confirm("투표 하시겠습니까?") == true){
+        alert("투표가 완료되었습니다");
+        document.getElementById("img1").src = "/static/img/onclick.png";
+    }
+    else{
+        return ;
+    }
+});
+
+$("#modal-open2").click(function(){
+    if(confirm("투표 하시겠습니까?") == true){
+        alert("투표가 완료되었습니다");
+        document.getElementById("img2").src = "/static/img/onclick.png";
+    }
+    else{
+        return ;
+    }
+});
+
+$("#modal-open3").click(function(){
+    if(confirm("투표 하시겠습니까?") == true){
+        alert("투표가 완료되었습니다");
+        document.getElementById("img3").src = "/static/img/onclick.png";
+    }
+    else{
+        return ;
+    }
+});
+
+$("#modal-open4").click(function(){
+    if(confirm("투표 하시겠습니까?") == true){
+        alert("투표가 완료되었습니다");
+        document.getElementById("img4").src = "/static/img/onclick.png";
+    }
+    else{
+        return ;
+    }
+});
+
+$("#modal-open5").click(function(){
+    if(confirm("투표 하시겠습니까?") == true){
+        alert("투표가 완료되었습니다");
+        document.getElementById("img5").src = "/static/img/onclick.png";
+    }
+    else{
+        return ;
+    }
+});
+
+$("#modal-open6").click(function(){
+    if(confirm("투표 하시겠습니까?") == true){
+        alert("투표가 완료되었습니다");
+        document.getElementById("img6").src = "/static/img/onclick.png";
+    }
+    else{
+        return ;
+    }
+});
+
+$("#modal-open7").click(function(){
+    if(confirm("투표 하시겠습니까?") == true){
+        alert("투표가 완료되었습니다");
+        document.getElementById("img7").src = "/static/img/onclick.png";
+    }
+    else{
+        return ;
+    }
+});
+
+$("#modal-open8").click(function(){
+    if(confirm("투표 하시겠습니까?") == true){
+        alert("투표가 완료되었습니다");
+        document.getElementById("img8").src = "/static/img/onclick.png";
+    }
+    else{
+        return ;
+    }
+});
