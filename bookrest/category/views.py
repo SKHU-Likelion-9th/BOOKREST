c2d5b5c17c2b30116b1b4932e22e47b7b00cf84d
kimjiin <kj980926@naver.com>
2021-08-07 12:19:45 +0900

    [ 수정 ] 학부별 필터링

--- bookrest/category/views.py
+++ bookrest/category/views.py
@@ -17,4 +17,10 @@ def category(request):
     page = request.GET.get('page', '') #몇번째 페이지인지 받아옴
     book_list = paginator.get_page(page) #posts
 
-    return render(request, 'category.html', {'book_list':book_list, 'search':search })
+    major_인문 = BookClassInfo.objects.filter(department='인문융합자율학부')
+    major_사회 = BookClassInfo.objects.filter(department='사회융합자율학부')
+    major_미컨 = BookClassInfo.objects.filter(department='미디어콘텐츠융합자율학부')
+    major_IT = BookClassInfo.objects.filter(department='IT융합자율학부')
+
+    return render(request, 'category.html', {'book_list':book_list, 'search':search,' major_인문':major_인문,'major_사회':major_사회
+        ,'major_미컨':major_미컨, 'major_IT':major_IT })
