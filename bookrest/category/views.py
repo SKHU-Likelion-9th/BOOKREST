f3ed13f929615873e0b448a2bddc4246b05fa705
kimjiin <kj980926@naver.com>
2021-08-07 18:37:49 +0900

    완성된 html 수정 ( 완료 : 카테고리, 메인 / 미완료 : 회원가입 )

--- bookrest/category/views.py
+++ bookrest/category/views.py
@@ -2,6 +2,7 @@ from django.core import paginator
 from django.shortcuts import render
 from .models import BookClassInfo
 from django.core.paginator import Paginator
+#from .filters import BookMajorFilter
 
 # Create your views here.
 # 검색과 페이징
@@ -9,18 +10,29 @@ def category(request):
     
     books = BookClassInfo.objects.all()
     search = request.GET.get('search', '')
+    sort_val = books
 
     if search:
         books = books.filter(title__icontains=search)
+        sort_val = books
 
-    paginator = Paginator(books, 9)
+    major = request.GET.get('major', '')
+    if major == "인문":
+        major_list = BookClassInfo.objects.filter(department='인문융합자율학부')
+        sort_val = major_list
+    if major == "사회":
+        major_list = BookClassInfo.objects.filter(department='사회융합자율학부')
+        sort_val = major_list
+    if major == "미컨":
+        major_list = BookClassInfo.objects.filter(department='미디어콘텐츠융합자율학부')
+        sort_val = major_list
+    if major == "IT":
+        major_list = BookClassInfo.objects.filter(department='IT융합자율학부')
+        sort_val = major_list
+
+    paginator = Paginator(sort_val, 9)
     page = request.GET.get('page', '') #몇번째 페이지인지 받아옴
     book_list = paginator.get_page(page) #posts
 
-    major_인문 = BookClassInfo.objects.filter(department='인문융합자율학부')
-    major_사회 = BookClassInfo.objects.filter(department='사회융합자율학부')
-    major_미컨 = BookClassInfo.objects.filter(department='미디어콘텐츠융합자율학부')
-    major_IT = BookClassInfo.objects.filter(department='IT융합자율학부')
 
-    return render(request, 'category.html', {'book_list':book_list, 'search':search,' major_인문':major_인문,'major_사회':major_사회
-        ,'major_미컨':major_미컨, 'major_IT':major_IT })
+    return render(request, 'category.html', {'book_list':book_list, 'search':search, 'major':major})
--- bookrest/join/forms.py
+++ bookrest/join/forms.py
@@ -10,22 +10,6 @@ class UserCreationForm(UserCreationForm):
         model = CustomUser
         fields = ("email", "password1", "password2", "username", "major", "student_id", "phone_number")
 
-    # def clean_password2(self):
-    #     # 두 비밀번호 입력 일치 확인
-    #     password1 = self.cleaned_data.get("password1")
-    #     password2 = self.cleaned_data.get("password2")
-    #     if password1 and password2 and password1 != password2:
-    #         raise forms.ValidationError("비밀번호가 맞지 않습니다. 다시 입력해주세요.")
-    #     return password2
-
-    # def save(self, commit=True):
-    #     # Save the provided password in hashed format
-    #     user = super(UserCreationForm, self).save(commit=False)
-    #     user.email = UserManager.normalize_email(self.cleaned_data['email'])
-    #     user.set_password(self.cleaned_data["password1"])
-    #     if commit:
-    #         user.save()
-    #     return user
 
 
 class UserChangeForm(forms.ModelForm):
