9c9385d51bb5c600e7d60c768a279bd8883e16ed
hasungYu <dbgktjd@naver.com>
2021-08-10 15:39:36 +0900

    detail 연결완료(HTML 수정중)

--- bookrest/category/views.py
+++ bookrest/category/views.py
@@ -7,19 +7,38 @@ from django.core.paginator import Paginator
 
 # Create your views here.
 # 검색과 페이징
+# Create your views here.
+# 검색과 페이징
 def category(request):
     
     books = BookClassInfo.objects.all()
     search = request.GET.get('search', '')
+    sort_val = books
 
     if search:
         books = books.filter(title__icontains=search)
+        sort_val = books
 
-    paginator = Paginator(books, 5)
+    major = request.GET.get('major', '')
+    if major == "인문":
+        major_list = BookClassInfo.objects.filter(department='인문융합자율학부')
+        sort_val = major_list
+    if major == "사회":
+        major_list = BookClassInfo.objects.filter(department='사회융합자율학부')
+        sort_val = major_list
+    if major == "미컨":
+        major_list = BookClassInfo.objects.filter(department='미디어콘텐츠융합자율학부')
+        sort_val = major_list
+    if major == "IT":
+        major_list = BookClassInfo.objects.filter(department='IT융합자율학부')
+        sort_val = major_list
+
+    paginator = Paginator(sort_val, 9)
     page = request.GET.get('page', '') #몇번째 페이지인지 받아옴
     book_list = paginator.get_page(page) #posts
 
-    return render(request, 'category.html', {'book_list':book_list, 'search':search })
+
+    return render(request, 'category.html', {'book_list':book_list, 'search':search, 'major':major})
     
 #detail 페이지 구현
 #detail page
