ccc8a90e2cb9571e4589ce5dabb7b241ab24b24f
hanseulhee <3021062@gmail.com>
2021-08-12 18:04:38 +0900

    Heroku 배포

--- bookrest/bookrest/settings.py
+++ bookrest/bookrest/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&fus%6eh$50oc=r$)t$$7ar2l+#$ly^2&u^*ev^0d4#&6$zvhp'
 
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&fus%6eh$50oc=r$)t$$7ar2l+#$ly^2&u^*ev^0d4#&6$zvhp')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -144,3 +145,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'join.CustomUser' #커스텀 유저 사용하겠다
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age = 500)
+DATABASES['default'].update(db_from_env)
